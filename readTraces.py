2ea54ba33e18553ee222dae886ba892ada990c91
Lucas Kinsey <88734358+LKINSEY@users.noreply.github.com>
2025-06-05 15:25:26 -0400

    does not work yet -- refactoring experiment summarization

--- readTraces.py
+++ readTraces.py
@@ -9,6 +9,7 @@ matplotlib.use('WebAgg') #display in web browser (requires a plt.show() and plt.
 # %matplotlib inline #go back to displaying inline
 import pandas as pd
 import os,glob, pickle
+from matplotlib.patches import Patch
 
 def sync_traces(expmtPath, dataDict):
     '''
@@ -78,11 +79,17 @@ def sync_traces(expmtPath, dataDict):
     return traceDict 
 
 def compare_all_ROIs(conditionIDX, trial, conditions, traces):
+
+
+
+
+    
     trial = trial-1 #because it goes in as +1 from init script
     actualTrial = trial+conditionIDX
     trialTraces = traces[trial+conditionIDX]
     voltageTrace = trialTraces[:,-1]
     conditionString = conditions[actualTrial]#expmtNotes['stim_type'].values[actualTrial]
+    
     stimFrame = find_stim_frame(voltageTrace, conditionString)
     if stimFrame is None:
         print('No Stimulation detected during this trial')
@@ -113,21 +120,219 @@ def compare_all_ROIs(conditionIDX, trial, conditions, traces):
         fig.tight_layout()
     return fig
 
-def summerize_experiment(expmtPath, traces):
+def summerize_experiment(expmtPath, dataDict):
     '''
-    trials:
-    dataDict.keys() = dict_keys([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
-    traces or features:
-    dataDict[0].keys() = dict_keys(['cycle0_traces', 'cycle1_traces', ...ect...  'T0_roiFeatures'])
-    traces == raw data
-    features of rois
-    dataDict[0]['T0_roiFeatures'].keys() = dict_keys(['roi1_redAvg', 'roi1_diameter', 'roi1_window', ... ect...
+    traces.keys() = 0, 'T0_roiOrder', 1, 'T1_roiOrder', ect...
+    traces[0] = np.array([
+        [roi1],
+        [roi2],
+        [roi3],
+        ect...
+    ])
+    traces['T0_roiOrder'] =  np.array([
+        2,
+        14, 
+        15,
+        ect...
+    ])
     '''
+    # traces = sync_traces(expmtPath, dataDict)
+    expmtNotes = pd.read_excel(glob.glob(expmtPath+'/expmtNotes*')[0])
+    slicePerTrial = expmtNotes['slice_label'].values
+    trialSets = np.unique(slicePerTrial)
+    conditions = expmtNotes['stim_type'].values
+    if -1 in trialSets:
+        trialSets = trialSets[1:]
+    trialPaths = np.array(glob.glob(expmtPath+'/TSeries*'))
+    nTrials = len(trialPaths)
+    trialIndices = np.arange(nTrials)
+    # setCounter = 1
+    print('Plotting...')
+    for trialSet in trialSets:
+        trialsInSet = trialPaths[slicePerTrial==trialSet]
+        segFN = glob.glob(expmtPath+f'/cellCountingTiffs/*slice{trialSet}_seg.npy')[0]
+        masksLoaded = np.load(segFN, allow_pickle=True).item()
+        masks = masksLoaded['masks']
+        #dependent on method set up as of 6-5-25
+        gcampROIs = np.unique(masks[2,:,:])[1:]
+        colabeledROIs = np.unique(masks[1,:,:])[1:]
+        gcampCenters = [center_of_mass(masks[2,:,:]==roi) for roi in gcampROIs]
+        colabeledCenters = [center_of_mass(masks[1,:,:]==roi) for roi in colabeledROIs]
+        firstTrialInSet = trialIndices[slicePerTrial==trialSet][0]
+        # if os.path.exists(expmt+'/figures/'):
+        #     pdfSummary = PdfPages(expmtPath+f'/figures/slice{slicePerTrial[trialIDX]}_dev_summary.pdf')
+        # else:
+        #     os.mkdir(expmt+'/figures/')
+        #     pdfSummary = PdfPages(expmtPath+f'/figures/slice{slicePerTrial[trialIDX]}_dev_summary.pdf')
+        maskIM = dataDict[f'T{firstTrialInSet}_masksIM']
+        if maskIM.shape[0] == 3:
+            maskIM = np.permute_dims(maskIM, (1,2,0))
+        outlinesIM = dataDict[f'T{firstTrialInSet}_outlinesIM']
+        outlinesIM = outlinesIM*2.2
+        if outlinesIM.shape[0] == 3:
+            outlinesIM = np.permute_dims(outlinesIM, (1,2,0))
+
+        fig, ax = plt.subplots(1,2)
+        ax[0].imshow(maskIM)
+        for roi in range(len(gcampROIs)):
+            ax[0].text(gcampCenters[roi][1]-2, gcampCenters[roi][0], f'{gcampROIs[roi]}', color='black', size=5, label='WGA-')
+        for roi in range(len(colabeledROIs)):
+            ax[0].text(colabeledCenters[roi][1]-2, colabeledCenters[roi][0], f'{colabeledROIs[roi]}', color='#FF4500', size=5)
+        ax[1].imshow(outlinesIM)
+        textColors = [
+            Patch(facecolor='#FF4500', edgecolor='#FF4500', label='WGA+'),
+            Patch(facecolor='black', edgecolor='black', label='WGA-')
+        ]
+        ax[0].axis='off'
+        ax[0].set_title('Mask Overlayed on GCaMP+ Cells')
+        ax[0].legend(handles=textColors, loc='upper right', title='ROI Colors')
+        ax[1].axis='off'
+        ax[1].set_title('ROI Outlines Overlayed on WGA Channel')
+        fig.suptitle(f'Slice {trialSet} Segmentations')
+        # return
+        for trialIDX in trialIndices[slicePerTrial==trialSet]:
+            print(f'\rTrial: {trialIDX}\n',
+                  f'\rSlice: {trialSet}\n',
+                  f'\rCondition: {conditions[trialIDX]}\n',
+                   end='',flush=True)
+            fig = compare_all_ROIs(conditions[trialIDX],)
+        return
+            
+            
+
+
+            
 
 
 
 
-    # trialPath = glob.glob(expmt+f'/TSeries*/rT{trial}*Ch2*.tif')[0]
+
+
+    
+
+
+def find_stim_frame(ventilatorTrace, condition):
+    edges = ventilatorTrace - np.roll(ventilatorTrace, -1)
+    risingIDX = np.where(edges<0)[0]
+    fallingIDX = np.where(edges>0)[0]
+    if condition == '5e':
+        exspLengths = fallingIDX - np.roll(fallingIDX,1)
+        longestExsp = np.unique(exspLengths)[-1]
+        stimFrame = fallingIDX[np.where(exspLengths == longestExsp)[0]-1]
+    elif condition=='baseline':
+        stimFrame = None
+        return
+    else:
+        inspLengths = risingIDX - np.roll(risingIDX, 1)
+        longestInsp = np.unique(inspLengths)[-1]
+        stimFrame = risingIDX[np.where(inspLengths == longestInsp)[0]-1]
+    return stimFrame[0]
+
+
+
+
+
+
+
+
+def plot_individual_cell(expmtPath, trial, roi, traces):
+    expmtNotes = pd.read_excel(glob.glob(expmtPath+'/expmtNotes*.xlsx')[0])
+    syncStatus = expmtNotes['stim_type'].values[0]
+    expmtConditions = np.unique(expmtNotes['stim_type'].values)
+    nConditions = len(expmtConditions)
+    nTrials = len(glob.glob(expmt+'/TSeries*'))
+    for trialSet in range(0,nTrials,nConditions):
+        if trial>trialSet and trial in range(trial, trialSet+nConditions):
+            fig,ax = plt.subplots(nConditions,1, sharex=True)
+            for condition in range(nConditions):
+                actualTrial = trialSet+condition
+                trialTraces = traces[trialSet+condition]
+                voltageTrace = trialTraces[:,-1]
+                conditionString = expmtNotes['stim_type'].values[actualTrial]
+                if syncStatus == 'voltage':
+                    stimFrame = find_stim_frame(voltageTrace, conditionString)
+                    if stimFrame is None:
+                        print('Baseline Epoch Skipped')
+                        break
+                    else:
+                        if stimFrame >=150:
+                            beggining = 150
+                        else:
+                            beggining = len(voltageTrace[:stimFrame])
+                        
+                        if stimFrame + 300 > len(voltageTrace):
+                            end = len(voltageTrace) - stimFrame
+                        else:
+                            end = 300
+                else:
+                    
+                    stimFrame = expmtNotes['stim_type'].values[actualTrial]
+                    
+                roiTrace = trialTraces[:,roi-1]
+                f0 = np.nanmean(roiTrace[stimFrame-beggining:stimFrame]) #mean is the average frames before the mechanical challenge
+                plottingTrace = roiTrace[stimFrame-beggining: stimFrame+end]
+                dFF = (plottingTrace - f0)/f0
+                if condition==0:
+                    ax[condition].plot(np.arange(-beggining, end), dFF, label='Raw Trace', color='green')
+                    ax[condition].set_ylabel(f'{conditionString}')
+                else:
+                    ax[condition].plot(np.arange(-beggining, end), dFF, color='green')
+                    ax[condition].set_ylabel(f'{conditionString}')
+                ax[condition].axvline(0, color='red')
+                
+                ax[condition].set_ylim([-0.5, 0.75])
+                fig.suptitle(f'ROI Traces of ROI={roi} at depth = {expmtNotes["z_pos"].values[actualTrial]}')
+                fig.supxlabel('Time (30 fps)')
+
+            fig.legend()
+    return fig
+
+#%
+
+if __name__=='__main__':
+    dataFrom = [
+        'U:/expmtRecords/Lucas*',
+        'C:/Analysis/april_data/Lucas*',
+        'U:/expmtRecords/res_galvo/Lucas*',
+        'U:/expmtRecords/mech_galvo/Lucas*',
+        ]
+    expmtRecords = glob.glob(dataFrom[3])
+    plt.close('all')
+    for expmt in expmtRecords:
+        print('Loading Traces for \n', expmt)
+        if os.path.exists(expmt+'/expmtTraces.pkl'):
+            if os.path.exists(expmt+'/figures/'):
+                print('Figures already generated for this experiment')
+            else:
+                with open(expmt+'/expmtTraces.pkl', 'rb') as f:
+                    dataDict = pickle.load(f)
+                # traces = sync_traces(expmt, dataDict)
+                plt.close('all')
+                summerize_experiment(expmt, dataDict)
+                plt.show()
+                break
+
+
+
+                # expmtNotes = pd.read_excel(glob.glob(expmt+'/expmtNotes*.xlsx')[0])
+                # expmtConditions = expmtNotes['stim_type']
+                # nTrials = len(expmtConditions)
+                # nConditions = len(np.unique(expmtConditions))
+                # traces = sync_traces(expmt, dataDict)
+                # with open(expmt+'/syncTrialTraces.pkl', 'rb') as f:
+                #     traces = pickle.dump(traces, f)
+                # print('Trialized traces extracted, now generating plots...')
+                # for trialSlice in range(0, nTrials, nConditions):
+                #     print(f'Generating Plots for trials {trialSlice} through {trialSlice + nConditions}')
+                #     summerize_experiment(expmt, trialSlice+1, traces)
+                # print('Saved Plots...')
+                # break #only work with 5-7 for now...
+        else:
+            print('Experiment not processed yet')
+        print('******************* NEXT *******************')
+
+# %%
+# trialPath = glob.glob(expmt+f'/TSeries*/rT{trial}*Ch2*.tif')[0]
     # loadedTif = tif.imread(trialPath)
     # mIM = np.nanmean(loadedTif, axis=0)
     # expmtNotes = pd.read_excel(glob.glob(expmtPath+'/expmtNotes*.xlsx')[0])
@@ -178,18 +383,18 @@ def summerize_experiment(expmtPath, traces):
     #     wgaIM[:,:,0] = np.power(   rmIM/np.max(rmIM-20) , .52)
     #     wgaIM[:,:,2] = np.power(   rmIM/np.max(rmIM-20) , .52)
 
-    #     fig, ax = plt.subplots( 1,2)
-    #     gCAMPIM = np.clip(gCAMPIM, 0,1)
-    #     ax[0].imshow(gCAMPIM)
-    #     ax[0].axis('off')
-    #     ax[0].set_title('GCaMP IM')
-    #     wgaIM = np.clip(wgaIM, 0,1)
-    #     ax[1].imshow(wgaIM)
-    #     ax[1].set_title('WGA TR IM')
-    #     ax[1].axis('off')
-    #     for roi in rois:
-    #         ax[0].text(roiCenters[roi-1][1]-2,roiCenters[roi-1][0], f'{roi}', color='black', size=2)
-    #     fig.suptitle(f'ROI {roi} Overlay')
+        # fig, ax = plt.subplots( 1,2)
+        # gCAMPIM = np.clip(gCAMPIM, 0,1)
+        # ax[0].imshow(gCAMPIM)
+        # ax[0].axis('off')
+        # ax[0].set_title('GCaMP IM')
+        # wgaIM = np.clip(wgaIM, 0,1)
+        # ax[1].imshow(wgaIM)
+        # ax[1].set_title('WGA TR IM')
+        # ax[1].axis('off')
+        # for roi in rois:
+        #     ax[0].text(roiCenters[roi-1][1]-2,roiCenters[roi-1][0], f'{roi}', color='black', size=2)
+        # fig.suptitle(f'ROI {roi} Overlay')
     #     plt.savefig(pdfSummary, format='pdf')
 
     # elif labeling == 'WGA594':
@@ -238,111 +443,3 @@ def summerize_experiment(expmtPath, traces):
     
     # print('Saving...')
     # pdfSummary.close()
-
-
-def find_stim_frame(ventilatorTrace, condition):
-    edges = ventilatorTrace - np.roll(ventilatorTrace, -1)
-    risingIDX = np.where(edges<0)[0]
-    fallingIDX = np.where(edges>0)[0]
-    if condition == '5e':
-        exspLengths = fallingIDX - np.roll(fallingIDX,1)
-        longestExsp = np.unique(exspLengths)[-1]
-        stimFrame = fallingIDX[np.where(exspLengths == longestExsp)[0]-1]
-    elif condition=='baseline':
-        stimFrame = None
-        return
-    else:
-        inspLengths = risingIDX - np.roll(risingIDX, 1)
-        longestInsp = np.unique(inspLengths)[-1]
-        stimFrame = risingIDX[np.where(inspLengths == longestInsp)[0]-1]
-    return stimFrame[0]
-
-def plot_individual_cell(expmtPath, trial, roi, traces):
-    expmtNotes = pd.read_excel(glob.glob(expmtPath+'/expmtNotes*.xlsx')[0])
-    expmtConditions = np.unique(expmtNotes['stim_type'].values)
-    nConditions = len(expmtConditions)
-    nTrials = len(glob.glob(expmt+'/TSeries*'))
-    for trialSet in range(0,nTrials,nConditions):
-        if trial>trialSet and trial in range(trial, trialSet+nConditions):
-            fig,ax = plt.subplots(nConditions,1, sharex=True)
-            for condition in range(nConditions):
-                actualTrial = trialSet+condition
-                trialTraces = traces[trialSet+condition]
-                voltageTrace = trialTraces[:,-1]
-                conditionString = expmtNotes['stim_type'].values[actualTrial]
-                stimFrame = find_stim_frame(voltageTrace, conditionString)
-                if stimFrame is None:
-                    print('Baseline Epoch Skipped')
-                    break
-                else:
-                    if stimFrame >=150:
-                        beggining = 150
-                    else:
-                        beggining = len(voltageTrace[:stimFrame])
-                    
-                    if stimFrame + 300 > len(voltageTrace):
-                        end = len(voltageTrace) - stimFrame
-                    else:
-                        end = 300
-                    
-                    roiTrace = trialTraces[:,roi-1]
-                    f0 = np.nanmean(roiTrace[stimFrame-beggining:stimFrame]) #mean is the average frames before the mechanical challenge
-                    plottingTrace = roiTrace[stimFrame-beggining: stimFrame+end]
-                    dFF = (plottingTrace - f0)/f0
-                    if condition==0:
-                        ax[condition].plot(np.arange(-beggining, end), dFF, label='Raw Trace', color='green')
-                        ax[condition].set_ylabel(f'{conditionString}')
-                    else:
-                        ax[condition].plot(np.arange(-beggining, end), dFF, color='green')
-                        ax[condition].set_ylabel(f'{conditionString}')
-                    ax[condition].axvline(0, color='red')
-                    
-                    ax[condition].set_ylim([-0.5, 0.75])
-                    fig.suptitle(f'ROI Traces of ROI={roi} at depth = {expmtNotes["z_pos"].values[actualTrial]}')
-                    fig.supxlabel('Time (30 fps)')
-
-            fig.legend()
-    return fig
-
-#%%
-
-if __name__=='__main__':
-    dataFrom = [
-        'U:/expmtRecords/Lucas*',
-        'C:/Analysis/april_data/Lucas*',
-        'U:/expmtRecords/res_galvo/Lucas*',
-        ]
-    expmtRecords = glob.glob(dataFrom[2])
-    plt.close('all')
-    for expmt in expmtRecords:
-        print('Loading Traces for \n', expmt)
-        if os.path.exists(expmt+'/expmtTraces.pkl'):
-            if os.path.exists(expmt+'/figures/'):
-                print('Figures already generated for this experiment')
-            else:
-                with open(expmt+'/expmtTraces.pkl', 'rb') as f:
-                    dataDict = pickle.load(f)
-                traces = sync_traces(expmt, dataDict)
-
-
-
-
-
-                # expmtNotes = pd.read_excel(glob.glob(expmt+'/expmtNotes*.xlsx')[0])
-                # expmtConditions = expmtNotes['stim_type']
-                # nTrials = len(expmtConditions)
-                # nConditions = len(np.unique(expmtConditions))
-                # traces = sync_traces(expmt, dataDict)
-                # with open(expmt+'/syncTrialTraces.pkl', 'rb') as f:
-                #     traces = pickle.dump(traces, f)
-                # print('Trialized traces extracted, now generating plots...')
-                # for trialSlice in range(0, nTrials, nConditions):
-                #     print(f'Generating Plots for trials {trialSlice} through {trialSlice + nConditions}')
-                #     summerize_experiment(expmt, trialSlice+1, traces)
-                # print('Saved Plots...')
-                # break #only work with 5-7 for now...
-        else:
-            print('Experiment not processed yet')
-        print('******************* NEXT *******************')
-
-# %%
