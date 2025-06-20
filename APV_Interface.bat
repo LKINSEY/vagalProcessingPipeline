ed076a6ac5a56b28d99ad805f18537d88969f95d
Lucas Kinsey <88734358+LKINSEY@users.noreply.github.com>
2025-06-20 12:40:52 -0400

    added better initial text for bat file

--- APV_Interface.bat
+++ APV_Interface.bat
@@ -1,4 +1,5 @@
-CALL
-python
+@echo off
+CALL "C:\Users\**\AppData\Local\miniconda3\Scripts\activate.bat" dataAnalysis %replace ** with usn, this is most likely the location of activate.bat
+python %wherever the python UI script exists
 CALL conda deactivate
 pause
