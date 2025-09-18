70b8520a88eb43f682adb805c41a9e319f1b3fbc
Lucas Kinsey <88734358+LKINSEY@users.noreply.github.com>
2025-09-18 13:53:36 -0400

    adding quick script to generate physiology pickle

---
+++ quick_generate_physiology_pickle.py
@@ -0,0 +1,37 @@
+#open appropriate file beforehand
+
+import labchart
+import win32com.client
+import matplotlib.pyplot as plt
+import pickle
+import matplotlib
+matplotlib.use('WebAgg') 
+
+expmtPathToSave = 'U:/expmtRecords/september2025/Lucas_250911_002/'
+
+app = labchart.Application()
+doc = app.active_document
+blockNum = 2
+dataDict = {}
+
+
+plt.close('all')
+for chanName in doc.channel_names:
+
+    keyName = chanName.replace(' ', '_')
+    result = doc.get_channel_data(chanName, blockNum)
+
+    dataDict[keyName+'_raw'] = result['y']
+    dataDict[keyName+'_start'] = result['start_time']
+    dataDict[keyName+'_end'] = result['end_time']
+    dataDict[keyName+'_fs'] = result['fs']
+    # dataDict[keyName+'_nTicks'] = int(result['fs']*result['end_time']) #just use the shape of raw data to get this
+    #troubleshooting/sanity checking - USE THIS TO MAKE SURE YOU ARE SAVING THE WRITE DATA!!!!
+#     plt.figure()
+#     plt.plot(result['y'])
+#     plt.title(chanName)
+# plt.show()
+
+#be sure to specify expmt or baseline
+with open(expmtPathToSave+'baselinePhysiology.pkl', 'wb') as f:
+    pickle.dump(dataDict, f)
